fccfa26e97da12b1f1b6ee3f162e5c5c269786ec
amrutahv <amruta.varudkar@gmail.com>
2017-09-16 19:01:16 -0700

    edited sctipt and environment. More listings (46338)

--- automatic_download.R
+++ automatic_download.R
@@ -8,23 +8,23 @@ url_list <- 'https://openapi.etsy.com/v2/listings/active?api_key=gxx1r5uo1zpe5c5
 
 pages1 <- list()
 
-for(i in 1:500){
+for(i in 1:501){
   mydata <- fromJSON(paste0(url_list,'&page=', i))
   message("Retrieving page ", i)
   pages1[[i]] <- mydata$results
   message(object.size(pages1))
   if(i == 100){
-    Sys.sleep(60)
     message("Waiting 1 min at", i)
+    Sys.sleep(60)
   } else if(i == 200){
-    Sys.sleep(60)
     message("Waiting 1 min at", i)
+    Sys.sleep(60)
   } else if(i == 300){
-    Sys.sleep(60)
     message("Waiting 1 min at", i)
+    Sys.sleep(60)
   } else if(i == 400){
-    Sys.sleep(60)
     message("Waiting 1 min at", i)
+    Sys.sleep(60)
   }
 }
 
@@ -35,8 +35,7 @@ new_data <- rbind_pages(pages1)
 uniq_data <- dplyr::anti_join(new_data, maj_data, by = "listing_id")
 
 
-big_data <- rbind(maj_data,big_data)
-
+big_data <- rbind(maj_data,uniq_data)
 
 
 
